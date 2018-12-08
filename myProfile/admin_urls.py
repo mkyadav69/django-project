937b929e27f28f76ad1d0d00ca9c0520dc61df85
mkyaadv59 <mkyadav59@gmail.com>
2018-12-08 19:15:22 +0530

    templating admin panel

--- myProfile/admin_urls.py
+++ myProfile/admin_urls.py
@@ -10,7 +10,18 @@
 
 # myProfile/urls.py
 from django.urls import path
-from .views.admin_views import AdminPageView
+from .views.admin_views import AdminPageView,AboutPageView,WorkPageView,EducationPageView,ExperiencePageView,SkillPageView,BlogPageView,ProjectPageView,VideoPageView,SongPageView,PersonalInfoPageView
+
 urlpatterns = [
     path('', AdminPageView.as_view(), name='adminManoj'),
+    path('about', AboutPageView.as_view(), name='about'),
+    path('blog', BlogPageView.as_view(), name='blog'),
+    path('education', EducationPageView.as_view(), name='education'),
+    path('experience', ExperiencePageView.as_view(), name='experience'),
+    path('personalInfo', PersonalInfoPageView.as_view(), name='personalInfo'),
+    path('skill', SkillPageView.as_view(), name='skill'),
+    path('song', SongPageView.as_view(), name='song'),
+    path('video', VideoPageView.as_view(), name='video'),
+    path('work', WorkPageView.as_view(), name='work'),
+    path('project', ProjectPageView.as_view(), name='project'),   
 ]
--- myProfile/views/admin_views.py
+++ myProfile/views/admin_views.py
@@ -14,3 +14,33 @@ from django.views.generic import TemplateView
 
 class AdminPageView(TemplateView):
     template_name = 'admin.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'admin/partials/about.html'
+
+class WorkPageView(TemplateView):
+    template_name = 'admin/partials/work.html'
+
+class EducationPageView(TemplateView):
+    template_name = 'admin/partials/education.html'
+
+class ExperiencePageView(TemplateView):
+    template_name = 'admin/partials/experience.html'
+
+class SkillPageView(TemplateView):
+    template_name = 'admin/partials/skill.html'
+
+class BlogPageView(TemplateView):
+    template_name = 'admin/partials/blog.html'
+
+class ProjectPageView(TemplateView):
+    template_name = 'admin/partials/project.html'
+
+class SongPageView(TemplateView):
+    template_name = 'admin/partials/song.html'
+
+class VideoPageView(TemplateView):
+    template_name = 'admin/partials/video.html'
+
+class PersonalInfoPageView(TemplateView):
+    template_name = 'admin/partials/personal_info.html'
